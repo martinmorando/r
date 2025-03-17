6ef3a0d6e8e05dd9569e7e4694e901379378aadf
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-17 19:03:46 -0300

    add 1-basics.r, add features of R to README

---
+++ 1-basics.r
@@ -0,0 +1,25 @@
+# Basics
+
+# Single line comment
+
+# No multi-line comment? :/
+
+# Output
+print("Hello Satoshi")  # "Hello Satoshi"
+print(21000000)         # 2.1e+07
+
+# Declare variable and assign string
+name <- "Satoshi Nakamoto"
+
+# Declare vector and assign values
+software <- c("Bitcoin", "Tor", "Tails", "PGP")
+
+# Comparison operators: ==, !=, >, >=, <, <=
+# Logical operators: &, |, !
+
+# Conditional
+if (2 == 1) {
+    print("2 = 1")
+} else {
+    print("2 != 1")
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# r
-Learning and experimenting with R
+# R
+
+- Created in 1993, it's free and open-source
+- Thousands of free packages available
+- Widely used for statistical analysis, data manipulation and data visualization
