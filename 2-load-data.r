e17b16a76921d63fe8eae699e3c9bbb6904ad70f
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-19 20:47:53 -0300

    add data types, and refactor comments

--- 2-load-data.r
+++ 2-load-data.r
@@ -3,15 +3,15 @@
 # Install package (just run once)
 #install.packages("readr")
 
-# Load library
+# Load readr library, to read CSV, TSV and generally delimited files in an efficient way
 library(readr)
 
-# Load data frame (think of a spreedsheat were R functions can be applied)
+# Load data frame (think of a spreadsheat were R functions can be applied)
 spx <- read_csv("data/1-spx.csv")
 
-# Preview first rows
-#head(spx)      # Displays 6 by default
-head(spx,10)    # Displays 10
+# Preview first rows 
+#head(spx)      # Displays 6 rows by default
+head(spx,10)    # Displays 10 rows
 
 # Generate summary statistics
 summary(spx)
