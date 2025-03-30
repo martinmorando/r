c6894c83a6e4cb1c97ff2d7eb1734666f5ccf095
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-30 14:36:29 -0300

    expand NA and refactor 002-variables.r for clarity

--- 002-variables.r
+++ 002-variables.r
@@ -1,21 +1,40 @@
 #
 #   Variables
+#   - It's possible to assign a value to a variable using:
+#       - Arrow sign (<-): most used
+#       - Equal sign (=)
+#   - class(var): returns the data type of var
 #
 
 # [VARIABLES & ASSIGNING VALUES]
-# - Alternative A: using the arrow sign ("<-")
-name <- "Satoshi Nakamoto" # Character data type (strings)
-n <- 2                     # Numeric data type (integers, decimals)
-m <- 4L                    # Integer data type (with "L")
-c <- 1 + 2i                # Complex data type (complex numbers)
-isDone <- TRUE             # Logical data type (boolean: TRUE, FALSE)
-rawData <- as.raw(0x42)    # Raw data type (bytes)
-wtf <- NA                  # Absence of value
+# - Character data type (strings)
+name <- "Satoshi Nakamoto" 
+print(class(name))          # Output: 'character'
 
-# - Alternative B: using the equal sign (=) (less used)
-q = 5
+# - Numeric data type (integers, decimals)
+number <- 2
+print(class(number))        # Output: 'numeric'
 
+# - Integer data type (with "L")
+integer <- 4L
+print(class(integer))       # Output: 'integer'
 
-# [RELATED FUNCTIONS]
-# - Check data type
-print( class(isDone) )
+# - Complex data type (complex numbers)
+complex <- 1 + 2i 
+print(class(complex))       # Output: 'complex'
+
+# - Logical data type (boolean: TRUE, FALSE)
+isDone <- TRUE             
+print(class(isDone))        # Output: 'logical'
+
+# - Raw data type (bytes)
+rawData <- as.raw(0x42)
+print(class(rawData))       # Output: 'raw'
+
+# - Absence of value (NA: Not Available): depends on the context
+absence <- NA                  
+print(class(absence))       # Output: 'logical'
+print(class(NA_character_)) # Output: 'character'
+print(class(NA_real_))      # Output: 'numeric'
+print(class(NA_integer_))   # Output: 'integer'
+print(class(NA_complex_))   # Output: 'complex'
